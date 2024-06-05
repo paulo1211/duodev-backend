a41b36f69834a624aec1903805b4ef61a166a3de
Paulo Ricardo <paulo.araujo@ozmap.com.br>
2024-06-05 08:41:22 -0300

    add new controllers

--- src/main/java/com/duodev/duodevbackend/controller/SessaoController.java
+++ src/main/java/com/duodev/duodevbackend/controller/SessaoController.java
@@ -1,6 +1,7 @@
 package com.duodev.duodevbackend.controller;
 
 import com.duodev.duodevbackend.dto.SessaoDto;
+import com.duodev.duodevbackend.model.Mentoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import com.duodev.duodevbackend.model.Sessao;
 import com.duodev.duodevbackend.service.SessaoService;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,18 +30,10 @@ public class SessaoController {
     return ResponseEntity.status(HttpStatus.CREATED).body(sessaoCriada);
   }
 
-
   @GetMapping("/{id}")
   public ResponseEntity<Sessao> obterSessao(@PathVariable Integer id) {
     Sessao sessao = sessaoService.getSessaoById(id);
     return ResponseEntity.ok(sessao);
-  }
-
-  @PutMapping("/{id}")
-  public ResponseEntity<String> atualizarSessao(@PathVariable int id, @RequestBody Sessao sessao) throws IOException {
-    String sessaoAtualizada = sessaoService.updateSessao(id, sessao);
-    return ResponseEntity.ok(sessaoAtualizada);
-
   }
 
   @DeleteMapping("/{id}")
@@ -53,5 +47,36 @@ public class SessaoController {
 
   }
 
+    @GetMapping("/mentoria/{id}")
+    public ResponseEntity<List<Sessao>> obterSessoesPorMentoria(@PathVariable Mentoria mentoria) {
+        List<Sessao> sessoes = sessaoService.getSessoesByMentoria(mentoria);
+        return ResponseEntity.ok(sessoes);
+    }
+
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<Sessao>> obterSessoesPorStatus(@PathVariable String status) {
+        List<Sessao> sessoes = sessaoService.getSessoesByStatus(status);
+        return ResponseEntity.ok(sessoes);
+    }
+
+    @GetMapping("/dataHoraInicial/{dataHoraInicial}")
+    public ResponseEntity<List<Sessao>> obterSessoesPorDataHoraInicial(@PathVariable String dataHoraInicial) {
+        List<Sessao> sessoes = sessaoService.findByDataHoraInicial(LocalDateTime.parse(dataHoraInicial));
+        return ResponseEntity.ok(sessoes);
+    }
+
+    @GetMapping("/dataHoraFinal/{dataHoraFinal}")
+    public ResponseEntity<List<Sessao>> obterSessoesPorDataHoraFinal(@PathVariable String dataHoraFinal) {
+        List<Sessao> sessoes = sessaoService.findByDataHoraFinal(LocalDateTime.parse(dataHoraFinal));
+        return ResponseEntity.ok(sessoes);
+    }
+
+    @GetMapping("/dataHoraInicial/{dataHoraInicial}/dataHoraFinal/{dataHoraFinal}")
+    public ResponseEntity<List<Sessao>> obterSessoesPorDataHoraEntre(@PathVariable String dataHoraInicial,
+                                                                     @PathVariable String dataHoraFinal) {
+        List<Sessao> sessoes = sessaoService.findByDataHoraInicialBetween(LocalDateTime.parse(dataHoraInicial),
+                LocalDateTime.parse(dataHoraFinal));
+        return ResponseEntity.ok(sessoes);
+    }
 
 }
