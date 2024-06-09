bd66bd95491d1604d73f24df3574092d464302cf
Paulo Ricardo <paulo.araujo@ozmap.com.br>
2024-06-09 15:57:17 -0300

    add search by email

--- src/main/java/com/duodev/duodevbackend/repository/UsuarioRepository.java
+++ src/main/java/com/duodev/duodevbackend/repository/UsuarioRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    @Query("SELECT u FROM Usuario u WHERE u.email = ?1")
+    Usuario findByEmail(String email);
 }
